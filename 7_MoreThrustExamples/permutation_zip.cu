8b936599fd36dfbe7e41adc0f4b565feb29ad5bc
Larter <m35sosnicki@gmail.com>
2015-12-04 10:23:37 +0100

    fixes fixes

---
+++ 7_MoreThrustExamples/permutation_zip.cu
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/zip_iterator.h>
+#include <thrust/reduce.h>
+
+struct int_float_multi_sum_operator
+{
+	__device__ __host__
+	float operator()(float prev_sum, thrust::tuple<int,float> current )
+	{
+		return prev_sum+get<0>(current)*get<1>(current);
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+	thrust::device_vector<int> ints(300);
+
+	for(int i=0; i < inst.size();++i)
+	{
+		ints[i]=i;
+	}
+
+	thrust::device_vector<float> floats(300);
+	for(int i=0; i < floats.size();++i)
+	{
+		floats[i]=floats.size()-i;
+	}
+
+
+
+	float sum = thrust::reduce(make_zip_iterator(ints.begin(), floats.begin()), make_zip_iterator(ints.end(), floats.end()),0, int_float_multi_sum_operator());
+
+	std::cout<<sum<<std::endl;
+	return 0;
+}
+}
